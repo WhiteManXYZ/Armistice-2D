7151963702b64c947f6185fa61211b6011f8e490
WhiteManXYZ <vladimir.gavrilyuk10@gmail.com>
2026-07-13 22:44:03 +0200

    simple move system added

--- scripts/MoveComponent.cs
+++ scripts/MoveComponent.cs
@@ -2,15 +2,22 @@ using Godot;
 
 namespace Armistice.scripts;
 
-public partial class MoveComponent : Node
+public partial class MoveComponent : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	[Export] public float Speed = 300f;
+	
+	public override void _PhysicsProcess(double delta)
 	{
-	}
+		var direction = Vector2.Zero;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		if (Input.IsActionPressed("move_right")) direction.X += 1;
+		if (Input.IsActionPressed("move_left"))  direction.X -= 1;
+		if (Input.IsActionPressed("move_down"))  direction.Y += 1;
+		if (Input.IsActionPressed("move_up"))    direction.Y -= 1;
+		
+		direction = direction.Normalized();
+		
+		Velocity = direction * Speed;
+		MoveAndSlide();
 	}
 }
